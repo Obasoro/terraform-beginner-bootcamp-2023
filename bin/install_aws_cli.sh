9537442669bb91339fafd582b01ea3ac18efd6ee
Obasoro <obasorokunle@gmail.com>
2023-09-22 23:04:51 +0000

    update

--- bin/install_aws_cli.sh
+++ bin/install_aws_cli.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
 
-PROJECT_ROOT='workspace/terraform-beginner-bootcamp-2023'
+PROJECT_ROOT='/workspace/terraform-beginner-bootcamp-2023'
 
 rm --f '/workspace/awscliv2.zip'
 
-rm -f '/workspace/aws'
+rm -rf '/workspace/aws'
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
@@ -12,7 +12,7 @@ sudo ./aws/install
 
 aws sts get-caller-identity
 
-#cd $PROJECT_ROOT
+cd $PROJECT_ROOT
 
-cd /
+
 
