e567acc9a27af495169dc461e4fef5ec2067d97f
Obasoro <obasorokunle@gmail.com>
2023-09-24 16:41:16 +0000

    update

--- bin/install_aws_cli.sh
+++ bin/install_aws_cli.sh
@@ -1,39 +1,18 @@
 #!/bin/env bash
 
-# Set the project root directory
 PROJECT_ROOT='/workspace/terraform-beginner-bootcamp-2023'
 
-# Check if AWS CLI is already installed
-if ! command -v aws &> /dev/null; then
-    # Download and install AWS CLI
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
-    sudo ./aws/install
-else
-    echo "AWS CLI is already installed."
-fi
+cd /workspace
 
-# Check AWS CLI version
-aws --version
+rm -f /workspace/awscliv2
+rm -rf /workspace/aws
 
-# Get AWS caller identity
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 
+unzip awscliv2.zip
+sudo ./aws/install
+
 aws sts get-caller-identity
 
-# Change directory to the project root
-if [ -d "$PROJECT_ROOT" ]; then
-    cd "$PROJECT_ROOT" || exit 1
-    echo "Changed directory to $PROJECT_ROOT"
-else
-    echo "Project root directory '$PROJECT_ROOT' not found."
-    exit 1
-fi
-
-# Add your additional commands or actions here
-
-# Example: Run Terraform commands, etc.
-
-# End of the script
+cd $PROJECT_ROOT
 
 cd /workspace/terraform-beginner-bootcamp-2023
-
-
