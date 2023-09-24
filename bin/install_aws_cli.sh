1943f61509d21503b57724cbc9d010e91f405e6f
Obasoro <obasorokunle@gmail.com>
2023-09-24 16:25:55 +0000

    update

--- bin/install_aws_cli.sh
+++ bin/install_aws_cli.sh
@@ -2,10 +2,6 @@
 
 PROJECT_ROOT='/workspace/terraform-beginner-bootcamp-2023'
 
-rm --f '/workspace/awscliv2.zip'
-
-rm -f '/workspace/aws'
-
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" -y
 unzip awscliv2.zip
 sudo ./aws/install
