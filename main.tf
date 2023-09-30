a247cb54d4093766b505320a4ff9a95e118b8f22
Obasoro <obasorokunle@gmail.com>
2023-09-30 07:28:34 -0500

    12-s3-bucket connection (#13)

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ terraform {
       source = "eda-dev-test/random"
       version = "6.6.8"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
   }
 }
 
@@ -16,7 +20,18 @@ provider "random" {
 resource "random_string" "bucket_name" {
   length           = 16
   special          = false
+  upper            = false
+  lower            = true
   override_special = "/@£$"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 
 
